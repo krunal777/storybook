8c29888b36d6991cf332f349865932f62fcdcaaa
krunal777 <codeandcore@gmail.com>
2023-10-04 13:31:43 +0530

    file upload and show v1.1

--- storybook/faceswap/urls.py
+++ storybook/faceswap/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-  path('',views.homePage)
+  path('',views.homePage),
+  path('success', views.success, name='success'),
+  path('hotel_images', views.display_hotel_images, name = 'hotel_images'),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- storybook/faceswap/views.py
+++ storybook/faceswap/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
+from .forms import HotelForm
+from .models import Hotel
 
 # Create your views here.
-def homePage(request):
-    return render(request,"index.html")
+# def homePage(request):
+#     return render(request,"index.html")
 
-def index(request):
-    return HttpResponse("Hello word")
+def homePage(request):
+    if request.method == 'POST':
+        form = HotelForm(request.POST, request.FILES)
+ 
+        if form.is_valid():
+            form.save()
+            return redirect('/hotel_images')
+    else:
+        form = HotelForm()
+      
+    return render(request, 'index.html', {'form': form})
+   
+def success(request):
+    return HttpResponse('successfully uploaded')
+
+
+def display_hotel_images(request):
+ 
+    if request.method == 'GET':
+        # getting all the objects of hotel.
+        Hotels = Hotel.objects.all()
+        return render(request, 'display_hotel_images.html',{'hotel_images': Hotels})
